4355035ba37b61e93bc8c3fa1563ec96d90d199d
Graham Fairweather <xotic750@gmail.com>
2019-07-22 15:19:31 +0200

    :bookmark: v3.0.6
    Dependency updates

--- dist/object-create-x.js
+++ dist/object-create-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-21T23:19:09.848Z",
+  "date": "2019-07-22T13:19:24.585Z",
   "describe": "",
   "description": "Sham for Object.create",
   "file": "object-create-x.js",
   "hash": "196426046fa168be59c4",
   "license": "MIT",
-  "version": "3.0.5"
+  "version": "3.0.6"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
